c8d20e48517f37cb73230bca7179169314394b1a
phennyagero-spec <phennyagero@gmail.com>
2026-06-02 19:08:04 +0300

    Add files via upload

---
+++ Control-flow.js
@@ -0,0 +1,9 @@
+let marks = 65;
+
+if (marks >= 70) {
+    console.log("Grade A");
+} else if (marks >= 50) {
+    console.log("Grade B");
+} else {
+    console.log("Grade C");
+}
---
+++ If statements.js
@@ -0,0 +1,7 @@
+let temperature = 30;
+
+if (temperature > 25) {
+    console.log("It is sunny today");
+} else {
+    console.log("It is cold today");
+}
